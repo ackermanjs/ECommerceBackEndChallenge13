a1cd030f8fddc145cb3c0da903418039b0a32716
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-03-21 17:17:21 -0600

    worked with a tutor to resolve some issues with my routes and models, everything seems to be working now, doing the insomnia recording now

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
-// const categoryRoutes = require('./category-routes');
-// const productRoutes = require('./product-routes');
-// const tagRoutes = require('./tag-routes');
-const apiRoutes = require("./api");
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
+// const apiRoutes = require("./api");
 
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Incorrect Route!</h1>");
-});
+// router.use((req, res) => {
+//   res.send("<h1>Incorrect Route!</h1>");
+// });
 
-// router.use('/categories', categoryRoutes);
-// router.use('/products', productRoutes);
-// router.use('/tags', tagRoutes);
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-    include: [{ model: Category }, { model: Tag }],
+    include: [{ model: Category }, { model: Tag, through: ProductTag }],
   })
     .then((productResults) => res.json(productResults))
     .catch((err) => res.status(500).json(err));
